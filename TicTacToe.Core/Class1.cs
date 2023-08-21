db0370a148631fc47472f643b3fc20ea5d5ff8f1
Ethan Lewis <lewisrm001@gmail.com>
2023-08-21 20:24:51 +0100

    first commit

---
+++ TicTacToe.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TicTacToe.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TicTacToe.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TicTacToe.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
